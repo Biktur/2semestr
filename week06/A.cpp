d736258c59e9437538d6764147ea672ca66fccf1
Artur <biktimirov.am@phystech.edu>
2024-03-14 14:43:52 +0300

    update week06

--- week06/A.cpp
+++ week06/A.cpp
@@ -10,23 +10,13 @@ struct Student
 void sort_by_name(Student students[], int size) {
   for (int i = 0; i < size; ++i) {
     for (int j = i; j > 0; --j) {
-      int letter_index = 0;
-      int is_less = 1;
-      while (1) {
-        if (static_cast<int>((students[j].name)[letter_index]) < static_cast<int>((students[j-1].name)[letter_index])) {
-          Student temp = students[j];
-          students[j] = students[j-1];
-          students[j-1] = temp;
-          break;
-        }
-        if (static_cast<int>((students[j].name)[letter_index]) > static_cast<int>((students[j-1].name)[letter_index])) {
-          is_less = 0;
-          break;
-        }
-        ++letter_index;
-        std::cout << letter_index << std::endl;
+      if (students[j].name < students[j-1].name) {
+        Student temp = students[j];
+        students[j] = students[j-1];
+        students[j-1] = temp;
+        continue;
       }
-      if (is_less == 0) {
+      if (students[j].name >= students[j-1].name) {
         break;
       }
     }
@@ -61,7 +51,7 @@ int main() {
   sort_by_name(students, N);
   sort_by_mark(students, N);
   for (int i = 0; i < N; ++i) {
-    if (students[i].mark >= M) {
+    if (students[i].mark > M) {
       std::cout << students[i].name << ' ';
     }
   }
--- week06/D.cpp
+++ week06/D.cpp
@@ -3,33 +3,75 @@
 struct Polynomial {
   int size = 0;
   int *polynomial = nullptr;
-  Polynomial(int n, int *P) {
-    size = n, polynomial = P;
+  Polynomial(int n) {
+    size = n, polynomial = new int[n * 2];
   }
 };
 
-Polynomial input_polynomial(int* polynomial, int size) {
-  polynomial = new int[size * 2];
+Polynomial input_polynomial(int size) {
+  Polynomial P = Polynomial(size);
   for (int i = 0; i < 2 * size; ++i) {
-    std::cin >> polynomial[i]; 
+    std::cin >> P.polynomial[i]; 
   }
-  return Polynomial(size, polynomial);
+  return P;
 }
 
 Polynomial multiply_polynomials(Polynomial P1, Polynomial P2) {
-  int *ptr = new int[P1.size * P2.polynomial[0] + 1];
-  for (int i = 0, i < P1[Polynomial])
+  Polynomial preresult = Polynomial(P1.size * P2.size);
+  for (int i = 0; i < P1.size; ++i) {
+    for (int j = 0; j < P2.size; ++j) {
+      preresult.polynomial[(i * P2.size + j) * 2] = P1.polynomial[i * 2] * P2.polynomial[j * 2];
+      preresult.polynomial[(i * P2.size + j) * 2 + 1] = P1.polynomial[i * 2 + 1] + P2.polynomial[j * 2 + 1];
+    }
+  }
 
+  for (int i = preresult.size - 1; i >= 0; --i) {
+    for (int j = i; j < preresult.size - 1; ++j) {
+      if (preresult.polynomial[2 * j + 1] < preresult.polynomial[2 * j + 3]) {
+        int temp_power = preresult.polynomial[2 * j + 1], temp_coefficient = preresult.polynomial[2 * j];
+        preresult.polynomial[2 * j + 1] = preresult.polynomial[2 * j + 3], preresult.polynomial[2 * j] = preresult.polynomial[2 * j + 2];
+        preresult.polynomial[2 * j + 3] = temp_power, preresult.polynomial[2 * j + 2] = temp_coefficient;
+        continue;
+      }
+      if (preresult.polynomial[2 * j + 1] == preresult.polynomial[2 * j + 3]) {
+        int temp_power = preresult.polynomial[2 * j + 1];
+        preresult.polynomial[2 * j + 1] = preresult.polynomial[2 * j + 3];
+        preresult.polynomial[2 * j + 3] = temp_power;
+        preresult.polynomial[2 * j + 2] += preresult.polynomial[2 * j], preresult.polynomial[2 * j] = 0;
+        continue;
+      }
+      break;
+    }
+  }
+  int n_result = 0;
+  for (int i = 0; i < preresult.size; ++i) {
+    if (preresult.polynomial[2 * i] != 0) {
+      ++n_result;
+    }
+  }
+  Polynomial result = Polynomial(n_result);
+  int result_idx = 0;
+  for (int i = 0; i < preresult.size; ++i) {
+    if (preresult.polynomial[2 * i] != 0) {
+      result.polynomial[2 * result_idx + 1] = preresult.polynomial[2 * i + 1];
+      result.polynomial[2 * result_idx] = preresult.polynomial[2 * i];
+      ++result_idx;
+    }
+  }
+  return result;
 }
 
 int main() {
   int n1 = 0;
   std::cin >> n1;
-  int *ptr1 = nullptr;
-  Polynomial P1 = Polynomial(n1, ptr1);
+  Polynomial P1 = input_polynomial(n1);
   
   int n2 = 0;
   std::cin >> n2;
-  int *ptr2 = nullptr;
-  Polynomial P2 = Polynomial(n2, ptr2);
+  Polynomial P2 = input_polynomial(n2);
+  Polynomial P1P2 = multiply_polynomials(P1, P2);
+  std::cout << P1P2.size << ' ';
+  for (int i = 0; i < P1P2.size * 2; ++i) {
+    std::cout << P1P2.polynomial[i] << ' ';
+  }
 }
